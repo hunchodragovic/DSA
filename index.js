a56771f2d3e9c7a027ee97fefef60c39823b3ad8
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-03-10 04:03:55 +0100

    gg

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const StudentNamesArr = ["John", "Jane", "Doe", "Smith", "Alex"];
-function getStudentName(allStudents, StudentName) {
-  for (let i = 0; i < StudentNamesArr.length; i++) {
-    const user = StudentNamesArr[i];
-    if (user === StudentName) {
-      return `found the user: ${user}`;
-    }
-  }
-}
-console.log(getStudentName(StudentNamesArr, "")); // Doe
+console.log(1 / 0);
