d6131f7bc2f2a8b537778370c801d65297f1965f
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-03-15 00:42:10 +0100

    Trees

---
+++ trees.js
@@ -0,0 +1,14 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+//
+
+const root = new BinaryTreeNode(10);
+root.left = new BinaryTreeNode(5);
+root.right = new BinaryTreeNode(15);
+
+console.log(root);
